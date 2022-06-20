c1fe63cf6f95246d15638d9ba8b733c35168d17d
ohju96 <ohju96@naver.com>
2022-06-20 22:46:01 +0900

    [#16] feature : 삭제2

--- demo/src/main/java/com/example/demo/controller/MainController.java
+++ demo/src/main/java/com/example/demo/controller/MainController.java
@@ -1,25 +1,6 @@
 package com.example.demo.controller;
 
 public class MainController {
-    int i = 10;
-
-    int j = 12;
-    int df=2;
-
-    int q = 20;
-    int wqer = 21;
-    int qd = 22;
-    int test = 1;
-
-    int t2 = 20;
-
-    int ttt = 10;
-
-    int t12 = 0;
-
-    int t14 = 20;
-
-    int j = 20;
 
     int t14 = 20;
 
